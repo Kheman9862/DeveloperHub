f77e1de5abb03701cdf688b76412fa8b8dc0175f
kheman9862 <garg.kheman1996@gmail.com>
2020-10-23 00:52:31 -0400

    Applied Backend Authenticcation and validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,17 +4,31 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
 //Load User Model
 const User = require("../../models/User");
+const passport = require("passport");
 
 // get
 router.get("/test", (req, res) => res.json({ msg: "Users work" }));
 
 // post Creating account
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then((user) => {
     if (user) {
-      return res.status(400).json("Email aready exists");
+      errors.email = "Email already exist";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: 200, //size
@@ -67,7 +81,7 @@ router.post("/login", (req, res) => {
           { expiresIn: 3600 },
           (err, token) => {
             res.json({
-              succes: true,
+              success: true,
               token: "Bearer " + token,
             });
           }
@@ -79,4 +93,18 @@ router.post("/login", (req, res) => {
   });
 });
 
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email,
+    });
+  }
+);
 module.exports = router;
